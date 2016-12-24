4b45db577f551540a1c4c906f9131a88de235478
amr-swalha <amr.swalha.code@outlook.com>
2016-12-24 17:58:06 +0200

    Adding the File Upload Module Example Code

---
+++ FileUploadMain/View.ascx
@@ -0,0 +1,7 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="View.ascx.cs" Inherits="Christoc.Modules.FileUploadMain.View" %>
+<asp:FileUpload ID="FileUpload1" runat="server" />
+<asp:Button ID="Button1" runat="server" Text="Upload" OnClick="Button1_Click1" />
+<asp:Button ID="Button2" runat="server" Text="Extract" OnClick="Button2_Click" />
+<p>
+    &nbsp;</p>
+<asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
---
+++ FileUploadMain/View.ascx.cs
@@ -0,0 +1,134 @@
+﻿/*
+' Copyright (c) 2016  Christoc.com
+'  All rights reserved.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+' TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+' THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+' CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+' DEALINGS IN THE SOFTWARE.
+' 
+*/
+
+using System;
+using DotNetNuke.Security;
+using DotNetNuke.Services.Exceptions;
+using DotNetNuke.Entities.Modules;
+using DotNetNuke.Entities.Modules.Actions;
+using DotNetNuke.Services.Localization;
+using ICSharpCode.SharpZipLib.Zip;
+using System.IO;
+using ICSharpCode.SharpZipLib.Core;
+namespace Christoc.Modules.FileUploadMain
+{
+    /// -----------------------------------------------------------------------------
+    /// <summary>
+    /// The View class displays the content
+    /// 
+    /// Typically your view control would be used to display content or functionality in your module.
+    /// 
+    /// View may be the only control you have in your project depending on the complexity of your module
+    /// 
+    /// Because the control inherits from FileUploadMainModuleBase you have access to any custom properties
+    /// defined there, as well as properties from DNN such as PortalId, ModuleId, TabId, UserId and many more.
+    /// 
+    /// </summary>
+    /// -----------------------------------------------------------------------------
+    public partial class View : FileUploadMainModuleBase, IActionable
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+
+            }
+            catch (Exception exc) //Module failed to load
+            {
+                Exceptions.ProcessModuleLoadException(this, exc);
+            }
+        }
+
+        public ModuleActionCollection ModuleActions
+        {
+            get
+            {
+                var actions = new ModuleActionCollection
+                    {
+                        {
+                            GetNextActionID(), Localization.GetString("EditModule", LocalResourceFile), "", "", "",
+                            EditUrl(), false, SecurityAccessLevel.Edit, true, false
+                        }
+                    };
+                return actions;
+            }
+        }
+       
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            ExtractZipFile(Server.MapPath("~/UploadedFiles/Upload.zip"), "", Server.MapPath("~/UploadedFiles/out/"));
+        }
+        public void ExtractZipFile(string archiveFilenameIn, string password, string outFolder)
+        {
+            ZipFile zf = null;
+            try
+            {
+                FileStream fs = File.OpenRead(archiveFilenameIn);
+                zf = new ZipFile(fs);
+                if (!String.IsNullOrEmpty(password))
+                {
+                    zf.Password = password;     // AES encrypted entries are handled automatically
+                }
+                foreach (ZipEntry zipEntry in zf)
+                {
+                    if (!zipEntry.IsFile)
+                    {
+                        continue;           // Ignore directories
+                    }
+                    String entryFileName = zipEntry.Name;
+                    // to remove the folder from the entry:- entryFileName = Path.GetFileName(entryFileName);
+                    // Optionally match entrynames against a selection list here to skip as desired.
+                    // The unpacked length is available in the zipEntry.Size property.
+
+                    byte[] buffer = new byte[4096];     // 4K is optimum
+                    Stream zipStream = zf.GetInputStream(zipEntry);
+
+                    // Manipulate the output filename here as desired.
+                    String fullZipToPath = Path.Combine(outFolder, entryFileName);
+                    string directoryName = Path.GetDirectoryName(fullZipToPath);
+                    if (directoryName.Length > 0)
+                        Directory.CreateDirectory(directoryName);
+
+                    // Unzip file in buffered chunks. This is just as fast as unpacking to a buffer the full size
+                    // of the file, but does not waste memory.
+                    // The "using" will close the stream even if an exception occurs.
+                    using (FileStream streamWriter = File.Create(fullZipToPath))
+                    {
+                        StreamUtils.Copy(zipStream, streamWriter, buffer);
+                    }
+                }
+            }
+            finally
+            {
+                if (zf != null)
+                {
+                    zf.IsStreamOwner = true; // Makes close also shut the underlying stream
+                    zf.Close(); // Ensure we release resources
+                }
+            }
+        }
+
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+            if (FileUpload1.HasFile)
+            {
+                FileUpload1.SaveAs(Server.MapPath("~/UploadedFiles/Upload.zip"));
+                Label1.Text = FileUpload1.PostedFile.FileName;
+            }
+            else
+            {
+                Label1.Text = "Please select a file";
+            }
+        }
+    }
+}
